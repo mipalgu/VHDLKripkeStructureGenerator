8cbf79cebf7613fb54b3d117fa0ca96d8678a28e
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-05-08 22:01:47 +1000

    Linted files.

--- Sources/VHDLKripkeStructureGenerator/VHDLKripkeStructureGenerator.swift
+++ Sources/VHDLKripkeStructureGenerator/VHDLKripkeStructureGenerator.swift
@@ -1,6 +1,9 @@
+/// Default struct.
 public struct VHDLKripkeStructureGenerator {
+    /// Hello World!
     public private(set) var text = "Hello, World!"
 
+    /// Default init.
     public init() {
     }
 }
--- Tests/VHDLKripkeStructureGeneratorTests/VHDLKripkeStructureGeneratorTests.swift
+++ Tests/VHDLKripkeStructureGeneratorTests/VHDLKripkeStructureGeneratorTests.swift
@@ -1,7 +1,9 @@
-import XCTest
 @testable import VHDLKripkeStructureGenerator
+import XCTest
 
+/// Default test class.
 final class VHDLKripkeStructureGeneratorTests: XCTestCase {
+    /// Default test.
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
