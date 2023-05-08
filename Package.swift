f62b2cb00db1ef05314ae396e91f46a37e18b74a
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-05-08 22:06:55 +1000

    Added docc.

--- Package.swift
+++ Package.swift
@@ -9,11 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VHDLKripkeStructureGenerator",
-            targets: ["VHDLKripkeStructureGenerator"]),
+            targets: ["VHDLKripkeStructureGenerator"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
