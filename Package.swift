17365b194b89c70452e0d3b02857aa49fb23c8c2
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-05-23 07:26:21 +1000

    Updated machines to version 1.0.0.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-        .package(url: "https://github.com/mipalgu/VHDLMachines.git", from: "0.1.1"),
+        .package(url: "https://github.com/mipalgu/VHDLMachines.git", from: "1.0.0"),
         .package(url: "https://github.com/mipalgu/VHDLParsing.git", from: "1.1.0")
     ],
     targets: [
