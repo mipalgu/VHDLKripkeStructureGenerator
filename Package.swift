b5979c2cf2edfe1b2477a90eb618591a23683163
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-08-16 12:21:30 +1000

    Updated VHDLParsing to use main branch.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
         .package(url: "https://github.com/mipalgu/VHDLMachines.git", from: "1.0.0"),
-        .package(url: "https://github.com/mipalgu/VHDLParsing.git", from: "1.1.0")
+        .package(url: "https://github.com/mipalgu/VHDLParsing.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
