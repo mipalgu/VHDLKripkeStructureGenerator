520bd403d4b23be0ff75b74b2bf3c25d65563368
Morgan McColl <morgan.mccoll92@gmail.com>
2024-04-16 23:10:20 +1000

    Bumped VHDLMachines to 4.0.0.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
-        .package(url: "https://github.com/mipalgu/VHDLMachines.git", from: "3.0.0"),
+        .package(url: "https://github.com/mipalgu/VHDLMachines.git", from: "4.0.0"),
         .package(url: "https://github.com/mipalgu/VHDLParsing.git", from: "2.4.0"),
         .package(url: "https://github.com/CPSLabGU/SwiftUtils.git", from: "0.1.0")
     ],
