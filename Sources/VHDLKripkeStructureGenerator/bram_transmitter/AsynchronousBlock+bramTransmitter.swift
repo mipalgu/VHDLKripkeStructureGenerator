71965ae2264d91259b7481f4663ea1e69b444e27
Morgan McColl <morgan.mccoll92@gmail.com>
2024-05-07 01:18:37 +1000

    Started implementing bram transmitter.

---
+++ Sources/VHDLKripkeStructureGenerator/bram_transmitter/AsynchronousBlock+bramTransmitter.swift
@@ -0,0 +1,120 @@
+// AsynchronousBlock+bramTransmitter.swift
+// VHDLKripkeStructureGenerator
+// 
+// Created by Morgan McColl.
+// Copyright © 2024 Morgan McColl. All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// 3. All advertising materials mentioning features or use of this
+//    software must display the following acknowledgement:
+// 
+//    This product includes software developed by Morgan McColl.
+// 
+// 4. Neither the name of the author nor the names of contributors
+//    may be used to endorse or promote products derived from this
+//    software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// -----------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or
+// modify it under the above terms or under the terms of the GNU
+// General Public License as published by the Free Software Foundation;
+// either version 2 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see http://www.gnu.org/licenses/
+// or write to the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA  02110-1301, USA.
+
+import VHDLMachines
+import VHDLParsing
+
+extension AsynchronousBlock {
+
+    init<T>(bramTransmitterFor representation: T) where T: MachineVHDLRepresentable {
+        let interface = Entity(bramInterfaceFor: representation)
+        let mappings = interface.port.signals.map {
+            VariableMap(
+                lhs: .variable(reference: .variable(name: $0.name)),
+                rhs: .expression(value: .reference(variable: .variable(reference: .variable(name: $0.name))))
+            )
+        }
+        let interfaceMap = PortMap(variables: mappings)
+        let uart = Entity.uartTransmitter
+        let uartMappings = uart.port.signals.map {
+            VariableMap(
+                lhs: .variable(reference: .variable(name: $0.name)),
+                rhs: .expression(value: .reference(variable: .variable(reference: .variable(name: $0.name))))
+            )
+        }
+        self = .blocks(blocks: [
+            .statement(statement: .assignment(
+                name: .variable(reference: .variable(name: .address)),
+                value: .expression(value: .cast(operation: .stdLogicVector(expression: .reference(
+                    variable: .variable(reference: .variable(name: .currentAddress))
+                ))))
+            )),
+            .statement(statement: .assignment(
+                name: .variable(reference: .variable(name: .finishedGeneration)),
+                value: .expression(value: .reference(variable: .variable(
+                    reference: .variable(name: .finished)
+                )))
+            )),
+            .component(
+                block: ComponentInstantiation(label: .bramInst, name: interface.name, port: interfaceMap)
+            ),
+            .component(
+                block: ComponentInstantiation(
+                    label: .uartInst, name: .uartTransmitter, port: PortMap(variables: uartMappings)
+                )
+            ),
+            .component(block: ComponentInstantiation(
+                label: .baudInst,
+                name: .baudGenerator,
+                port: PortMap(variables: [
+                    VariableMap(
+                        lhs: .variable(reference: .variable(name: .clk)),
+                        rhs: .expression(value: .reference(variable: .variable(
+                            reference: .variable(name: .clk)
+                        )))
+                    ),
+                    VariableMap(
+                        lhs: .variable(reference: .variable(name: .pulse)),
+                        rhs: .expression(value: .reference(variable: .variable(
+                            reference: .variable(name: .baudPulse)
+                        )))
+                    )
+                ])
+            )),
+            .process(block: .bramTransmitter)
+        ])
+    }
+
+}
