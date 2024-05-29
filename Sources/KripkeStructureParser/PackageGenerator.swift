8551b6eb45898c609bdc60ceaad1b4cc255553c5
Morgan McColl <morgan.mccoll92@gmail.com>
2024-05-29 15:03:44 +1000

    Update Kripke structure version to 1.2.0 in generated package.

--- Sources/KripkeStructureParser/PackageGenerator.swift
+++ Sources/KripkeStructureParser/PackageGenerator.swift
@@ -191,7 +191,7 @@ extension String {
             dependencies: [
                 .package(url: "https://github.com/mipalgu/VHDLParsing.git", from: "2.4.0"),
                 .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-                .package(url: "https://github.com/cpslabgu/VHDLKripkeStructures.git", from: "1.1.0")
+                .package(url: "https://github.com/cpslabgu/VHDLKripkeStructures.git", from: "1.2.0")
             ],
             targets: [
                 .target(
