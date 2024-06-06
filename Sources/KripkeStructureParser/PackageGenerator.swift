b13719a398530b572a7bbf1e6c79940f9fb3e35f
Morgan McColl <morgan.mccoll92@gmail.com>
2024-06-06 13:49:27 +1000

    Bumped kripke structure versions.

--- Sources/KripkeStructureParser/PackageGenerator.swift
+++ Sources/KripkeStructureParser/PackageGenerator.swift
@@ -192,7 +192,7 @@ extension String {
             dependencies: [
                 .package(url: "https://github.com/mipalgu/VHDLParsing.git", from: "2.4.0"),
                 .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-                .package(url: "https://github.com/cpslabgu/VHDLKripkeStructures.git", from: "1.2.0")
+                .package(url: "https://github.com/cpslabgu/VHDLKripkeStructures.git", from: "1.3.0")
             ],
             targets: [
                 .target(
